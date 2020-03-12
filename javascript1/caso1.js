f2656e871e5c0bb5b7082fc9b5ef0cb764a93943
ighing20 <ighing20@gmail.com>
2020-03-12 12:56:08 -0500

    final de presentancion

--- javascript1/caso1.js
+++ javascript1/caso1.js
@@ -1,13 +1,16 @@
 function mostrar(form) {
   var inputs = form.elements;
   var xmldata = ['<cliente>'];
+  var telefono = document.getElementById("telefono").value;
+  console.log(telefono);
   for (var i = 0; i < inputs.length; i++) {
     if (inputs[i].name) {
-      xmldata.push("<", inputs[i].name, ">");
+      xmldata.push(`<${inputs[i].name}>`);
       xmldata.push(inputs[i].value);      
       xmldata.push("</", inputs[i].name, ">");
     }
   }
   xmldata.push('</cliente>');
   alert(xmldata.join(""));
+  //alert(xmldata);
 }
