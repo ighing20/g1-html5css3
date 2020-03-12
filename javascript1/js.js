cf50acc3e6d225b3e13c7597fe38c3306788fd10
jocaos <jcarhuasp@gmail.com>
2020-03-12 10:51:02 -0500

    Caso 1

--- javascript1/js.js
+++ javascript1/js.js
@@ -1,6 +1,13 @@
 function mostrar(form) {
-    var inputs = form.elements;
-    for(var i=0; i < inputs.length; i++){
-        alert(inputs[i].value);
+  var inputs = form.elements;
+  var xmldata = ['<cliente>'];
+  for (var i = 0; i < inputs.length; i++) {
+    if (inputs[i].name) {
+      xmldata.push("<", inputs[i].name, ">");
+      xmldata.push(inputs[i].value);      
+      xmldata.push("</", inputs[i].name, ">");
     }
+  }
+  xmldata.push('</cliente>');
+  alert(xmldata.join(""));
 }
